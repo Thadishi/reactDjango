6823b6735156c103c38bcd346d465f807525a29a
Thabo Kopane <thsdishi@gmail.com>
2019-05-05 06:55:12 +0200

    react app works on its own, django app works on its own

---
+++ authapp/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Router from './src/Router';
+
+export default class App extends Component{
+  componentWillMount(){
+    axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+    axios.defaults.timeout = 1500;
+  }
+  render() {
+    return (
+      <Router />
+    );
+  }
+}
---
+++ authapp/src/components/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {View, Text} from 'react-native';
+import LoginOrCreateForm from './common/LoginOrCreateForm';
+
+class Login extends Component{
+    render(){
+        return (
+            <View style={{ flex: 1 }}>
+                <LoginOrCreateForm />
+            </View>
+        );
+    }
+}
+
+export default Login;
